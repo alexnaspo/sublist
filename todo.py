7ccd636f72a7f5a5e26b1f1013ece39d41325b3e
Alexander Naspo <alex.naspo@gmail.com>
2013-02-27 22:23:11 -0500

    panel selection now opens file

--- todo.py
+++ todo.py
@@ -1,5 +1,4 @@
 import sublime, sublime_plugin, os, threading
-from time import sleep
 
 toDoList = []
 
@@ -9,7 +8,7 @@ class TodoCommand(sublime_plugin.TextCommand):
     directory = '/Users/Alex/Sites/clients/Bothsider'
     thread = UpdateList(directory)
     thread.start()
-    sleep(1.00)
+
 
 class UpdateList(threading.Thread):
   def __init__(self, directory):
@@ -25,7 +24,7 @@ class UpdateList(threading.Thread):
             for num, line in enumerate(searchfile, 0):
               if "@todo" in line:          
                 fullPath = os.path.join(dirname, filename)       
-                item = AddListItem(fullPath, line, num)
+                item = ListItem(fullPath, line, num)
                 item.run()
             searchfile.close()
             # print os.path.join(dirname, filename)      
@@ -34,7 +33,7 @@ class UpdateList(threading.Thread):
     except ( 4 ) as (e):
       err = "here"
 
-class AddListItem():
+class ListItem():
   def __init__(self, filepath, text, lineNum):
     self.filepath = filepath
     self.text = text
@@ -56,7 +55,8 @@ class TodolistCommand(sublime_plugin.TextCommand):
       
     window.show_quick_panel(curList, self.open, sublime.MONOSPACE_FONT)
 
-  def open(self, edit):
+  def open(self, index):
     window = sublime.active_window()
-    print('yes')
+    window.open_file(toDoList[index].filepath)
+    
 
