e112d88b106cc0b79b8eb33e6d7f65c12f5cbe9f
Alexander Naspo <alex.naspo@gmail.com>
2013-02-27 19:50:25 -0500

    created UpdateList Class extending Thread library

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, threading
 # @todo - Finish project
 class TodoCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -7,6 +7,11 @@ class TodoCommand(sublime_plugin.TextCommand):
     window = sublime.active_window()
     view = window.active_view()
     print(view.file_name())
+
+    threads = []
+    thread = UpdateList()
+    thread.start()
+
 
     for dirname, dirnames, filenames in os.walk('/Users/Alex/Sites/clients/Bothsider/application'):
       # print path to all subdirectories first.
@@ -19,4 +24,13 @@ class TodoCommand(sublime_plugin.TextCommand):
           for line in searchfile:
             if "@todo" in line: print line
           searchfile.close()
-          print os.path.join(dirname, filename)
+          #print os.path.join(dirname, filename)
+
+class UpdateList(threading.Thread):
+  def run (self):
+    try:
+      print('yes')
+      return
+    except ( error ) as (e):
+      err = "here"
+
