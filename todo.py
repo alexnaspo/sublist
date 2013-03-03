bb7e45543b7b8f5c3f8f82cbc817cb26b940bbeb
Alexander Naspo <alex.naspo@gmail.com>
2013-03-03 00:21:39 -0500

    multiple directory lists created

--- todo.py
+++ todo.py
@@ -8,30 +8,27 @@ import json
 toDoList = []
 
 
-class GetdirCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        packages_path = sublime.packages_path()
-        mysessionpath = "{0}{1}..{1}Settings{1}Auto Save Session.sublime_session".format(packages_path, os.sep)
-        response = json.loads(open(mysessionpath).read(),strict=False)
-        for window in response['windows']:
-            if window['window_id'] == sublime.active_window().id():
-                print(window['folders'])
-
-
 class UpdatelistCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # @todo how to pass the directory?
-        directory = '/Users/Alex/Sites/clients/Bothsider/application'
+        dirs = getDirs()
         global toDoList
-        toDoList = List(directory)
-        # creates the listObject in a thread
-        toDoList.start()
+        for i, x in enumerate(dirs):
+            toDoList.append(List(x))
+            toDoList[i].start()
         sublime.status_message('AutoListr successfully loaded your list')
 
 
 class PanelCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        toDoList.panel()
+        window = sublime.active_window()
+
+        curList = []
+
+        for List in toDoList:
+            for item in List.list:
+                print(item.filepath)
+                curList.append([item.text, item.filepath])
+        window.show_quick_panel(curList, List.open, sublime.MONOSPACE_FONT, {"here": True})
 
 
 class ListItem():
@@ -39,6 +36,12 @@ class ListItem():
         self.filepath = filepath
         self.text = text
         self.lineNum = lineNum
+
+    # def open(self, index):
+    #     if (index == -1):
+    #         return
+    #     window = sublime.active_window()
+    #     window.open_file(self.filepath + ":" + str(self.lineNum + 1), sublime.ENCODED_POSITION)
 
 
 class List(threading.Thread):
@@ -75,6 +78,16 @@ class List(threading.Thread):
     def count(self):
         return len(self.list)
 
+    # def getDirs(self):
+    #     packages_path = sublime.packages_path()
+    #     mysessionpath = "{0}{1}..{1}Settings{1}Auto Save Session.sublime_session".format(packages_path, os.sep)
+    #     response = json.loads(open(mysessionpath).read(), strict=False)
+    #     for window in response['windows']:
+    #         if window['window_id'] == sublime.active_window().id():
+    #             for directory in window['folders']:
+    #                 print(directory['path'])
+    #                 self.dirs.append(directory['path'])
+
     def open(self, index):
         # User cancels panel
         if (index == -1):
@@ -82,13 +95,26 @@ class List(threading.Thread):
         window = sublime.active_window()
         window.open_file(self.list[index].filepath + ":" + str(self.list[index].lineNum + 1), sublime.ENCODED_POSITION)
 
-    def panel(self):
-        window = sublime.active_window()
-        if(self.count() < 1):
-            window.show_quick_panel(["No Items"], None, sublime.MONOSPACE_FONT)
-            return
-        curList = []
+    # def panel(self):
+    #     window = sublime.active_window()
+    #     if(self.count() < 1):
+    #         window.show_quick_panel(["No Items"], None, sublime.MONOSPACE_FONT)
+    #         return
+    #     curList = []
 
-        for item in self.list:
-            curList.append([item.text, item.filepath])
-        window.show_quick_panel(curList, self.open, sublime.MONOSPACE_FONT)
+    #     for item in self.list:
+    #         curList.append([item.text, item.filepath])
+    #     window.show_quick_panel(curList, self.open, sublime.MONOSPACE_FONT)
+
+
+def getDirs():
+    dirs = []
+    packages_path = sublime.packages_path()
+    mysessionpath = "{0}{1}..{1}Settings{1}Auto Save Session.sublime_session".format(packages_path, os.sep)
+    response = json.loads(open(mysessionpath).read(), strict=False)
+    for window in response['windows']:
+        if window['window_id'] == sublime.active_window().id():
+            for directory in window['folders']:
+                print(directory['path'])
+                dirs.append(directory['path'])
+            return dirs
