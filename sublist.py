709566a93bd36005d9622a6bd1d47a1786df1237
Alexander Naspo <alex.naspo@gmail.com>
2013-03-03 19:09:09 -0500

    moved settings to global variable

--- sublist.py
+++ sublist.py
@@ -5,16 +5,25 @@ import threading
 import re
 
 toDoList = []
+terms = []
+ignore = []
 
 # @TODO auto creation/completion of github/bitbucket issues?
 
 
 class UpdatelistCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-
-        global toDoList
+        global terms, ignore, toDoList
         toDoList = []
         dirs = sublime.active_window().folders()
+
+        s = sublime.load_settings("sublist.sublime-settings")
+
+        for x in s.get("terms"):
+            terms.append(x.encode("ascii", "ignore"))
+
+        for x in s.get("ignore_dirs"):
+            ignore.append(x.encode("ascii", "ignore"))
 
         for i, x in enumerate(dirs):
             #spawn thread for each top-level directory in project
@@ -23,7 +32,6 @@ class UpdatelistCommand(sublime_plugin.TextCommand):
             if dirs < 1:
                 # temporary fix, this should be re-done/re-thought properly
                 break
-        sublime.status_message('AutoListr successfully loaded your list')
 
 
 class PanelCommand(sublime_plugin.TextCommand):
@@ -32,6 +40,7 @@ class PanelCommand(sublime_plugin.TextCommand):
         curList = []
 
         if len(toDoList) > 1:
+            print "here"
             # multiple directories in project, add project select panel
             for List in toDoList:
                 curList.append(List.dir)
@@ -46,11 +55,12 @@ class PanelCommand(sublime_plugin.TextCommand):
             window.show_quick_panel(curList, toDoList[0].open, sublime.MONOSPACE_FONT)
 
     def project(self, index):
-        #project select panel
+        # project select panel
         window = sublime.active_window()
         curList = []
         for item in toDoList[index].list:
             curList.append([item.text, item.filepath])
+        print toDoList[index].list
         window.show_quick_panel(curList, toDoList[index].open, sublime.MONOSPACE_FONT)
 
 
@@ -65,30 +75,20 @@ class List(threading.Thread):
     def __init__(self, directory):
         self.list = []
         self.dir = directory
-        self.terms = []
-        self.ignore = []
-        s = sublime.load_settings("sublist.sublime-settings")
-        for x in s.get("terms"):
-            self.terms.append(x.encode("ascii", "ignore"))
-        threading.Thread.__init__(self)
-        for x in s.get("ignore_dirs"):
-            self.ignore.append(x.encode("ascii", "ignore"))
         threading.Thread.__init__(self)
 
     # creates a list
     def run(self):
         try:
             # search files for "@todo"
+            print self.dir
             for dirname, dirnames, filenames in os.walk(self.dir):
-                # ignore directories defined in settings to speed up search
-                if any(dirname == self.dir + x for x in self.ignore):
-                    break
                 for filename in filenames:
                     searchfile = open(os.path.join(dirname, filename), "r")
                     # @TODO create moving status bar
                     for num, line in enumerate(searchfile, 0):
                         #search each line for all strings defined in settings
-                        if any(x in line for x in self.terms):
+                        if any(x in line for x in terms):
                             fullPath = os.path.join(dirname, filename)
                             # @TODO regex should be based on settings
                             line = re.search("(@todo.*|@return.*)", line, re.I | re.S)
@@ -110,6 +110,7 @@ class List(threading.Thread):
 
     def open(self, index):
         # User cancels panel
+        print("cool")
         if (index == -1):
             return
         window = sublime.active_window()
